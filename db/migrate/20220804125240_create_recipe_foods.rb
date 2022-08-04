0b15dc4ee6b26e8a20e1d9a22d17641cebaa736e
Anucet Murhula <anicetmurhula20@gmail.com>
2022-08-04 21:33:07 +0200

    Update food model

--- db/migrate/20220804125240_create_recipe_foods.rb
+++ db/migrate/20220804125240_create_recipe_foods.rb
@@ -2,8 +2,8 @@ class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
       t.decimal :quantity
-      t.references :recipe, null: false, foreign_key: true
-      t.references :food, null: false, foreign_key: true
+      t.belongs_to :recipe, foreign_key: true
+      t.belongs_to :food, foreign_key: true
 
       t.timestamps
     end
