bf7db9ce10fd30192fab332bbe47afb46f71d30b
Anucet Murhula <anicetmurhula20@gmail.com>
2022-08-05 19:47:32 +0200

    Update recipe foods

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,8 +1,5 @@
 class RecipeFood < ApplicationRecord
-  belongs_to :recipe
-  belongs_to :food
-  validates :food, presence: true 
-  validates :recipe, presence: true
-  validates :quantity, :food_id, presence: true
+  belongs_to :recipe, :optional => true
+  belongs_to :food, :optional => true
   validates :quantity, numericality: { only_integer: true, greater_than: 0}
 end
